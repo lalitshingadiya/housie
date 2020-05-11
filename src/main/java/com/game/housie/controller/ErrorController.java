4c7a084780573de2ef2c3ef4baf34dfcbe15cbbd
Lalitkumar Shingadiya <lalit.shingadiya@eclinicalworks.com>
2020-05-11 09:08:38 -0400

    Removing compilation errors in ErrorController.java

--- src/main/java/com/game/housie/controller/ErrorController.java
+++ src/main/java/com/game/housie/controller/ErrorController.java
@@ -1,15 +1,10 @@
 package com.game.housie.controller;
 
-import jdk.management.resource.internal.ResourceNatives;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
